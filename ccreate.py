cf9ba504a6e140c6f78720fbc28318fefbf5ee99
TunaFishCan1 <39428374+TunaFishCan1@users.noreply.github.com>
2020-12-07 16:26:12 +0900

    Update ccreate.py

--- ccreate.py
+++ ccreate.py
@@ -4,35 +4,35 @@ import torch
 
 def create_cam(num):
     if num==1:
-        K=[[1124.83,0,1018.069],[0,1123.996,534.9457],[0,0,1]]
-        K=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in K])
-        R=[[-0.2572692,0.5643166,0.7844484],[-0.9308321,0.07331344,-0.3580177],[-0.259546,-0.8222968,0.5064227]]
-        R=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in R])
-        t=[[1181.555,-223.6565,3214.04]]
-        t=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in t])
-        name='54138969'
-    elif num==2:
         K=[[1124.563,0,967.7123],[0,1123.253,512.639],[0,0,1]]
         K=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in K])
         R=[[0.1483106,-0.9873533,-0.05601296],[-0.9800903,-0.1391867,-0.1415988],[0.1320118,0.07589837,-0.9883383]]
         R=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in R])
         t=[[-734.5952],[-345.3347],[4178.617]]
         t=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in t])
+        name='54138969'
+    elif num==2:
+        K=[[1118.958,0,971.2457],[0,1118.306,520.8431],[0,0,1]]
+        K=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in K])
+        R=[[0.3139955,-0.1641321,-0.9351296],[-0.9035717,-0.3540529,-0.2412564],[-0.2914875,0.9207101,-0.2594762]]
+        R=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in R])
+        t=[[-709.3732],[-889.1614],[5026.707]]
+        t=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in t])
         name='55011271'
     elif num==4:
-        K=[[1124.592,0,935.112],[0,1121.993,533.8148],[0,0,1]]
+        K=[[1122.902,0,913.9825],[0,1122.022,518.5341],[0,0,1]]
         K=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in K])
-        R=[[0.1610608,0.6487166,-0.7437918],[-0.8499792,-0.2918426,-0.4385924],[-0.5015923,0.7028475,0.5043912]]
+        R=[[-0.1943118,0.9434211,0.2686998],[-0.9006306,-0.06303116,-0.4299902],[-0.3887254,-0.3255515,0.8619216]]
         R=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in R])
-        t=[[1426.446],[-1043.317],[5750.805]]
+        t=[[870.4645],[-213.2622],[3788.815]]
         t=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in t])
         name='58860488'
     elif num==6:
-        K=[[1122.218,0,972.8661],[0,1120.647,528.6412],[0,0,1]]
+        K=[[1124.197,0,930.3889],[0,1122.969,578.2481],[0,0,1]]
         K=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in K])
-        R=[[-0.02079385,-0.9501867,0.3109871],[-0.9657519,-0.06137666,-0.2521037],[0.2586329,-0.3055786,-0.9163684]]
+        R=[[-0.1066995,-0.7382388,0.6660471],[-0.9860531,-0.007487193,-0.1662626],[0.1277283,-0.674498,-0.7271438]]
         R=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in R])
-        t=[[-591.5411],[71.836],[3680.45]]
+        t=[[-38.31519],[-394.2763],[3723.456]]
         t=numpy.array([numpy.array([numpy.float32(i) for i in lst]) for lst in t])
         name='60457274'
     return Camera(R,t,K,None,name)
